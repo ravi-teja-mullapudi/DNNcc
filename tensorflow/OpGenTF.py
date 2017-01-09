e2abbcfcd3976e6c9bd4db00a05c78b5cf255b32
Your Name <you@example.com>
2017-01-09 15:32:33 -0500

    WIP generating a tensorflow op interface

--- tensorflow/OpGenTF.py
+++ tensorflow/OpGenTF.py
@@ -17,7 +17,7 @@ attr_data_types = ["float", "double",
 
 def get_attr_strs(attrs):
     attr_strs = []
-    for att, val in attrs.iteritems():
+    for att, val in attrs:
         at_str = "Attr(\""
         if isinstance(val, basestring):
             assert(val in attr_data_types)
@@ -31,7 +31,7 @@ def get_attr_strs(attrs):
 
 def get_spec_strs(specs, prefix):
     spec_strs = []
-    for spec, val in specs.iteritems():
+    for spec, val in specs:
         spec_str = prefix + "(\""
         if isinstance(val, basestring):
             spec_str = spec_str + spec + ": " + val + "\")"
@@ -42,18 +42,42 @@ def get_spec_strs(specs, prefix):
         spec_strs.append(spec_str)
     return spec_strs
 
-def gen_reg_op_macro_str(op_name, attrs, inputs, outputs):
+def gen_reg_op_macro_str(op_name, attrs, inputs, outputs, shape_fn):
     macro_name = "REGISTER_OP" + "(\"" + op_name + "\")";
     macro_call = []
     macro_call.append(macro_name)
     macro_call.extend(get_attr_strs(attrs))
     macro_call.extend(get_spec_strs(inputs, "Input"))
     macro_call.extend(get_spec_strs(outputs, "Output"))
-
+    macro_call.extend(["Shape(" + shape_fn + ")"])
     macro_str = "\n.".join(macro_call)
     return macro_str
 
-def generate_op_registration(op_name, attrs, inputs, outputs):
+def gen_shape_fn(output_shapes):
+    shape_fn = []
+    shape_fn.append(c.Line(
+        "[](::tensorflow::shape_inference::InferenceContext* c)"))
+
+    func_body = []
+    for out, dims in output_shapes:
+        dim_strs = [ str(d) for d in dims ]
+        # dim initializer list
+        dim_init_list = "{" + ",".join(dim_strs) + "}"
+        # make a shape
+        shape_name = "s" + str(out)
+        func_body.append(c.Assign("auto " + shape_name,
+                                  "c->MakeShape(" + dim_init_list + ")"))
+        dim_set_str = "c->set_output(" + str(out) + ", " + shape_name + ")"
+        func_body.append(c.Statement(dim_set_str));
+
+    func_body.append(c.Statement("return Status::OK()"));
+    body = c.Block(func_body)
+
+    shape_fn.append(body)
+    return str(c.Module(shape_fn))
+
+def gen_op_registration(op_name, attrs, inputs, outputs,
+                        output_shapes, kernel_class_name):
     # Add the headers into the module
     contents = []
     contents.append(c.Include("tensorflow/core/framework/op.h", system = False))
@@ -64,20 +88,76 @@ def generate_op_registration(op_name, attrs, inputs, outputs):
     contents.append(c.Statement("using namespace tensorflow"))
     contents.append(c.Line())
 
+    shape_fn = gen_shape_fn(output_shapes);
+
     # Registration macro
-    reg_macro = gen_reg_op_macro_str(op_name, attrs, inputs, outputs)
+    reg_macro = gen_reg_op_macro_str(op_name, attrs, inputs, outputs, shape_fn)
     contents.append(c.Statement(reg_macro))
     return c.Module(contents)
 
-def generate_op_kernel():
+def gen_op_kernel_constructor(class_name):
+    constructor = []
+    const_sign = "explicit %s(OpKernelConstruction* context) : OpKernel(context)" % (class_name)
+    constructor.append(c.Line(const_sign))
+    constructor.append(c.Block([]))
+    return constructor
+
+def gen_op_kernel_compute_fn():
+    compute_fn = []
+    compute_fn.append(c.Line("void Compute(OpKernelContext* context) override"))
+    fn_body = []
+
+    inputs = [c.Comment("Booyah")]
+    fn_body.extend(inputs)
+    compute_fn.append(c.Block(fn_body))
+    return compute_fn
+
+def gen_op_kernel_class_defn(class_name):
+    class_defn = []
+    class_defn.append(c.Line("class " + class_name + ": public OpKernel"))
+
+    class_body = []
+    class_body.append(c.Line("public:"))
+
+    class_construct = gen_op_kernel_constructor(class_name)
+    class_body.extend(class_construct)
+
+    class_body.append(c.Line())
+    compute_fn = gen_op_kernel_compute_fn()
+    class_body.extend(compute_fn)
+
+    body = c.Block(class_body)
+    class_defn.append(body)
+    # TODO: fix this at some point
+    class_defn.append(c.Line(";"))
+    return class_defn
+
+def gen_op_kernel(kernel_class_name):
+    contents = []
+    contents.append(c.Include("tensorflow/core/framework/op_kernel.h", system = False))
+
+    # Name space declarations
+    contents.append(c.Line())
+    contents.append(c.Statement("using namespace tensorflow"))
+    contents.append(c.Line())
+
+    class_defn = gen_op_kernel_class_defn(kernel_class_name)
+
+    contents.extend(class_defn)
+    return c.Module(contents)
+
+def gen_op_python_wrapper():
     return
 
-def generate_op_python_wrapper():
+def gen_op_gradient():
     return
 
-def generate_op_gradient():
-    return
+print(gen_op_registration("ZeroOut",
+                          {},
+                          [("to_zero", "int32")],
+                          [("zeroed", "int32")],
+                          [(0, [1000])],
+                          "ZeroOutOp"
+                          ))
 
-print(generate_op_registration("ZeroOut", {},
-                                       {"to_zero" : "int32"},
-                                       {"zeroed": "int32"}))
+print(gen_op_kernel("ZeroOutOp"))
